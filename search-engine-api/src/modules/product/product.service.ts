a38c65f90e056fba31b81c8f7b49edc87860db79
Oniel1721 <oni1san7alv21@gmail.com>
2022-12-08 13:09:00 -0400

    search product service improved withpartialMatch

--- search-engine-api/src/modules/product/product.service.ts
+++ search-engine-api/src/modules/product/product.service.ts
@@ -1,10 +1,9 @@
 import { Product } from './product.interfaces'
 import { getAllProducts } from './product.repository'
+import { doesPartialMatch } from '../../utils/text-matcher'
 
 export const searchProducts = (query: string = ''): Product[]=>{
     const products = getAllProducts()
-    const results = products.filter(({title})=>{
-        return title.toLowerCase().includes(query.toLowerCase())
-    })
+    const results = products.filter(({title})=>doesPartialMatch(title, query))
     return results
 }
