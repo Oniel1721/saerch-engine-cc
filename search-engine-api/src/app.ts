7d1ec2805d38b1ceaca14b642c665574e5519fdb
Oniel1721 <oni1san7alv21@gmail.com>
2022-12-08 13:34:18 -0400

    product module ready and added to /api/products endpoint

--- search-engine-api/src/app.ts
+++ search-engine-api/src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import { productModule } from './modules'
+
 const app = express()
+
+app.use('/api/products', productModule)
 
 app.get('/', (req, res) => {
     res.send('Everything ok!')
