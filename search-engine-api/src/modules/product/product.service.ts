749bf4b5f81ebb0ffdf44dce9e964a09c2614b40
Oniel1721 <oni1san7alv21@gmail.com>
2022-12-08 12:39:49 -0400

    product service searchProducts working as expected

--- search-engine-api/src/modules/product/product.service.ts
+++ search-engine-api/src/modules/product/product.service.ts
@@ -2,5 +2,9 @@ import { Product } from './product.interfaces'
 import { getAllProducts } from './product.repository'
 
 export const searchProducts = (query: string = ''): Product[]=>{
-    return [getAllProducts()[0]]
+    const products = getAllProducts()
+    const results = products.filter(({title})=>{
+        return title.toLowerCase().includes(query.toLowerCase())
+    })
+    return results
 }
