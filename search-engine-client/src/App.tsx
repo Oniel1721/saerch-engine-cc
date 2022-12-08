bbd30924b74dbf64d83123d13e28b21f4d907d20
Oniel1721 <oni1san7alv21@gmail.com>
2022-12-08 15:11:33 -0400

    react-router-dom added to client with home page route

--- search-engine-client/src/App.tsx
+++ search-engine-client/src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+const routes = [
+  {
+    path: '/',
+    element: <h1>Products search engine</h1>
+  }
+]
 
 export const App = ()=>{
   return (
-    <main>
-        <h1>Products search engine</h1>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        {
+          routes.map(route=>(
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))
+        }
+      </Routes>
+    </BrowserRouter>
   )
 }
 
