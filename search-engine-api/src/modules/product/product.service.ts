11ac86877f1d42c5656f877004c275107db70956
Oniel1721 <oni1san7alv21@gmail.com>
2022-12-09 10:44:12 -0400

    api product service get by id test and implementation created

--- search-engine-api/src/modules/product/product.service.ts
+++ search-engine-api/src/modules/product/product.service.ts
@@ -1,9 +1,14 @@
 import { Product } from './product.interfaces'
-import { getAllProducts } from './product.repository'
+import { getAllProducts, getProductById as findProductById } from './product.repository'
 import { doesPartialMatch } from '../../utils/text-matcher'
 
 export const searchProducts = (query: string = ''): Product[]=>{
     const products = getAllProducts()
     const results = products.filter(({title})=>doesPartialMatch(title, query))
     return results
+}
+
+export const getProductById = (id: string): Product | null=>{
+    const product = findProductById(parseInt(id))
+    return product
 }
