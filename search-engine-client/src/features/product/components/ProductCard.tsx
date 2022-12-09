26f251c132c0d253760021f003144026d8f94bf0
Oniel1721 <oni1san7alv21@gmail.com>
2022-12-09 09:03:15 -0400

    test created for ProductCard component

---
+++ search-engine-client/src/features/product/components/ProductCard.tsx
@@ -0,0 +1,9 @@
+import { Product } from "../types"
+
+interface Props {
+    product: Product
+}
+
+export const ProductCard = ({ product }: Props)=>{
+    return null
+}
---
+++ search-engine-client/src/features/product/components/__test__/ProductCard.test.tsx
@@ -0,0 +1,36 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
+import { Product } from '../../types'
+import { ProductCard } from '../ProductCard'
+
+const product: Product = {
+    id: 1,
+    title: 'Some title',
+    description: 'Some description',
+    shortDescription: 'Some short description',
+    photo: 'Photo url'
+}
+
+describe('Searching', ()=>{
+    afterEach(cleanup)
+
+    it('Should render', ()=>{
+        render(<ProductCard product={product} />)
+    })
+
+    it('Should render title', async ()=>{
+        render(<ProductCard product={product} />)
+        await screen.findByText(product.title)
+    })
+
+    it('Should render shortDescription', async ()=>{
+        render(<ProductCard product={product} />)
+        await screen.findByText(product.shortDescription)
+    })
+
+    it('Should render an img with title as alt and photo url as src', async ()=>{
+        render(<ProductCard product={product} />)
+        const img = await screen.findByAltText(product.title) as HTMLImageElement
+        expect(img.src).toBe(product.photo)
+    })
+})
